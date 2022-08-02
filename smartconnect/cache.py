5d2f82e99f761c82d860ee6377646eac7b831204
jgoodheart <79868330+jgoodheart@users.noreply.github.com>
2022-08-02 10:55:40 -0400

    Caching changes
    add in flag for whether to use cache. we want each smart sync interval to grab new data so it can keep cache updated for routing to use
    
    stop using config.state since it is at risk of being updated by other integrations. using redis cache instead

--- smartconnect/cache.py
+++ smartconnect/cache.py
@@ -1,7 +1,23 @@
+import json
+
 import redis
+
 from smartconnect import smart_settings
 
+state_key_base = 'er.function.state'
 
 cache = redis.Redis(
     host=smart_settings.REDIS_HOST, port=smart_settings.REDIS_PORT, db=smart_settings.REDIS_DB
 )
+
+
+def save_poll_time(state: str, integration_id: str):
+    state_key = f'{state_key_base}.{integration_id}'
+    cache.set(state_key, state)
+
+
+def get_state(integration_id: str):
+    state = cache.get(f'{state_key_base}.{integration_id}')
+
+    return json.loads(state) if state else {}
+
